47f97bffe3b7520648aa82367ff5ff54976d40a1
do phuoc <dovanphuoc@gmail.com>
2020-11-25 21:49:05 +0700

    demo

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+$('.collection-slider .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 4
+        },
+        600: {
+            items: 4
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
+
+$('.coWork .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 50,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 8
+        },
+        600: {
+            items: 8
+        },
+        1000: {
+            items: 8
+        }
+    }
+})
